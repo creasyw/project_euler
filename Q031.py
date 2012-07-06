64e2a092158f93fc5e5ecdb259d7ca2f73b01145
creasyw <creasywuqiong@gmail.com>
2012-07-06 17:25:51 -0400

    Q031

---
+++ Q031.py
@@ -0,0 +1,20 @@
+# The solution is created by Zen Denton
+# http://zacharydenton.com/project-euler-solutions/31/
+
+def ways_to_change(target, coins):
+    if target == 0 or len(coins) == 1:
+        return 1
+    else:
+        coins.sort()
+        largest = coins[-1]
+        uses = target/largest
+        total = 0
+        for i in range(uses+1):
+            total += ways_to_change(target-largest*i, coins[:-1])
+        return total
+
+def main():
+    print ways_to_change(200, [1,2,5,10,20,50,100,200])
+
+if __name__=="__main__":
+    main()
