10845be4b9026e661cddaa9d55b7d60befb95cfb
creasyw <creasywuqiong@gmail.com>
2012-07-16 12:28:01 -0400

    Q026 add solution 2

--- Q026.py
+++ Q026.py
@@ -20,17 +20,29 @@ def runWithPrecision(num, pre):
 
     return longest
 
+def main():
+    # Main script
+    precision = 50
+    length = 0
+    number = 0
+    for num in range(2,1000):
+        fraction = str(runWithPrecision(num, precision))
+        if len(fraction) > length:
+            length = len(fraction)
+            number = num
+    
+    print "The longest fraction comes from %d with length %d" % (number, length)
 
-# Main script
-precision = 50
-length = 0
-number = 0
-for num in range(2,1000):
-    fraction = str(runWithPrecision(num, precision))
-    if len(fraction) > length:
-        length = len(fraction)
-        number = num
+def solution_2():
+    def recurring_cycle(n,d):
+        for t in range(1,d):
+            if 1 == 10**t%d:
+                return t
+        return 0
+    longest = max(recurring_cycle(1,i) for i in range(2, 1001))
+    print [i for i in range(2,1001) if recurring_cycle(1,i) == longest][0]
 
-print "The longest fraction comes from %d with length %d" % (number, length)
-
+if __name__=="__main__":
+    #main()
+    solution_2()
 
