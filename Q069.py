a1e504b82cff940ee98906beaed50ca0cdd18af7
creasyw <creasywuqiong@gmail.com>
2012-07-21 16:23:46 -0400

    move function 'factorize' to factor

--- Q069.py
+++ Q069.py
@@ -2,26 +2,7 @@
 
 import math
 from operator import mul
-
-def factorize(n):
-    res = []
-    # iterate over all even numbers first.
-    while n % 2 == 0:
-        res.append(2)
-        n //= 2
-    # try odd numbers up to sqrt(n)
-    limit = math.sqrt(n+1)
-    i = 3
-    while i <= limit:
-        if n % i == 0:
-            res.append(i)
-            n //= i
-            limit = math.sqrt(n+i)
-        else:
-            i += 2
-    if n != 1:
-        res.append(n)
-    return res
+from factor import factorize
 
 def totient(n):
     return n * reduce(mul, [1 - (1.0 / p) for p in set(factorize(n))])
--- factor.py
+++ factor.py
@@ -1,3 +1,24 @@
+def factorize(n):
+    import math
+    res = []
+    # iterate over all even numbers first.
+    while n % 2 == 0:
+        res.append(2)
+        n //= 2
+    # try odd numbers up to sqrt(n)
+    limit = math.sqrt(n+1)
+    i = 3
+    while i <= limit:
+        if n % i == 0:
+            res.append(i)
+            n //= i
+            limit = math.sqrt(n+i)
+        else:
+            i += 2
+    if n != 1:
+        res.append(n)
+    return res
+
 def pull_off_factors(n, p, output_list):
     count = 0
     while True:
