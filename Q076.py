fad7f37451053d457f4866dfb8236fd5f91f311d
creasyw <creasywuqiong@gmail.com>
2012-07-23 16:38:12 -0400

    Add comments about the better solution

--- Q076.py
+++ Q076.py
@@ -14,6 +14,10 @@ def solve_plus(target, nums):
 def main():
     print solve_plus(100, range(1,100))
 
+# The difference between this Q and Q031 is that Q031 only has limited choices to
+# construct the final number, but Q076 and Q079 have all possible choices less than
+# the given number to construct it! It becomes straightforward to use the math @
+# http://mathworld.wolfram.com/PartitionFunctionP.html
 def solution_2():
     from Q078 import pentagonal, partition
     generalized_pentagonals = sorted(map(pentagonal, range(-100,100)))[1:]
