d3aa0066bb225ec30c014e07bec59b9fdd00eacd
creasyw <creasywuqiong@gmail.com>
2013-04-03 01:29:27 -0500

    optimize python solution

--- Q087/Q087.py
+++ Q087/Q087.py
@@ -2,12 +2,13 @@ import os
 import sys
 sys.path.append(os.path.abspath('..'))
 import prime
+# product is impractical because of too many unnecessary calculation
+#from itertools import product
 
 def main():
     limit = 50000000
-    a = int(limit**0.5)
-    primes = prime.gen_prime(a)
-    sumlst = set()
+    primes = prime.gen_prime(int(limit**0.5))
+    result = set()
 
     for i in primes:
         for j in primes:
@@ -15,10 +16,9 @@ def main():
             if sum >= limit: break
             for k in primes:
                 sum2 = sum+k**4
-                if sum2 >= limit: break
-                sumlst.add(sum2)
-                #print i, j, k
-    print "Q086 = ", len(sumlst)
+                if sum2 < limit:
+                    result.add(sum2)
+    print "Q086 = ", len(result)
 
 
 if __name__=="__main__":
