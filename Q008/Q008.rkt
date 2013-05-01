dd62be7303153e1a762f2ef71ecf869ea723b9e6
creasyw <creasywuqiong@gmail.com>
2013-04-30 21:27:06 -0400

    Complete racket solution for Q008

--- Q008/Q008.rkt
+++ Q008/Q008.rkt
@@ -10,7 +10,21 @@
 
 (define (concatenate-number st)
   (define (helper acc)
-    (if (null? (st)) acc
-        (helper (string-append acc (car (st))))))
+    (let ((temp (st)))
+      (if (null? temp) (string->list acc)
+          (helper (string-append acc (car temp))))))
   (helper ""))
 
+(define (stlist->numlist slst)
+  (if (null? slst) '()
+      (cons (- (char->integer (car slst)) (char->integer #\0)) (stlist->numlist (cdr slst)))))
+
+(define (largest-product nlst)
+  (define (helper lst acc)
+    (if (> 5 (length lst)) acc
+        (letrec ((product (foldl * 1 (take lst 5))))
+          (helper (cdr lst)
+                  (if (> acc product) acc product)))))
+  (helper (list-tail nlst 5) (foldl * 1 (take nlst 5))))
+
+(largest-product (stlist->numlist (concatenate-number next-row)))
