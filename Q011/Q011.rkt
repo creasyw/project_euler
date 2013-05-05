1bb60c1f9ad00410124ebcb6317d5b0f464d7b5f
creasyw <creasywuqiong@gmail.com>
2013-05-04 19:51:58 -0500

    add function to find largest horizontal product

--- Q011/Q011.rkt
+++ Q011/Q011.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../text_processing.rkt")
+(require "../Q008/Q008.rkt")
 
 (define (string->lst filename)
   (let ((st (next-row filename)))
@@ -13,13 +14,26 @@
             (helper (append acc (list (process-string (string-split (car temp)) '())))))))
     (helper '())))
 
+(define (max-element lst)
+  (define (helper itr result)
+    (cond ((null? itr) result)
+          ((> (car itr) result) (helper (cdr itr) (car itr)))
+          (#t (helper (cdr itr) result))))
+  (helper lst 0))
 
+(define (hproduct lstoflst)
+  (define (helper lst result)
+    (if (null? lst) result
+        (letrec ((temp (car lst))
+                 (local (largest-product temp 4)))
+          (if (> local result) (helper (cdr lst) local) (helper (cdr lst) result)))))
+  (helper lstoflst 0))
 
 ;(define (largest-product filename sidelen)
 ;  (letrec ((lst (concatenate-number (next-row filename)))
 ;           (vlargest (vproduct lst))
 ;           (hlargest (hprodouct lst))
 ;           (dlargest (dproduct lst)))
-;    (max (list vlargest hlargest dlargest))))
+;    (max-element (list vlargest hlargest dlargest))))
     
     
