9edf7a38d552e51385c5e135ebd872a7e81da53b
creasyw <creasywuqiong@gmail.com>
2013-05-04 19:52:48 -0500

    Add api for external calling

--- Q008/Q008.rkt
+++ Q008/Q008.rkt
@@ -2,16 +2,18 @@
 
 (require "../text_processing.rkt")
 
+(provide largest-product)
+
 (define (stlist->numlist slst)
   (if (null? slst) '()
       (cons (- (char->integer (car slst)) (char->integer #\0)) (stlist->numlist (cdr slst)))))
 
-(define (largest-product nlst)
+(define (largest-product nlst num)
   (define (helper lst acc)
-    (if (> 5 (length lst)) acc
-        (letrec ((product (foldl * 1 (take lst 5))))
+    (if (> num (length lst)) acc
+        (letrec ((product (foldl * 1 (take lst num))))
           (helper (cdr lst)
                   (if (> acc product) acc product)))))
-  (helper (list-tail nlst 5) (foldl * 1 (take nlst 5))))
+  (helper (list-tail nlst num) (foldl * 1 (take nlst num))))
 
-(largest-product (stlist->numlist (concatenate-number (next-row "Q008.txt"))))
+; (largest-product (stlist->numlist (concatenate-number (next-row "Q008.txt"))) 5)
