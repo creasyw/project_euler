3877ba127d65d22a8a6da27d3cda677b9806f06a
creasyw <creasywuqiong@gmail.com>
2013-05-29 00:44:25 -0500

    add max-element and min-element

--- list.rkt
+++ list.rkt
@@ -1,5 +1,7 @@
 #lang racket
 (provide num-to-intlist)
+(provide max-element)
+(provide min-element)
 
 ;; returns a list of integers containing every digit of the "num"
 (define (num-to-intlist num)
@@ -8,3 +10,15 @@
       (if (null? lst) (reverse acc)
           (helper (cdr lst) (cons (- (char->integer (car lst)) (char->integer #\0)) acc))))
     (helper temp '())))
+
+(define (max-element lst)
+  (define (helper result rest)
+    (if (null? rest) result
+        (helper (max result (car rest)) (cdr rest))))
+  (helper -inf.0 lst))
+
+(define (min-element lst)
+  (define (helper result rest)
+    (if (null? rest) result
+        (helper (min result (car rest)) (cdr rest))))
+  (helper +inf.0 lst))
