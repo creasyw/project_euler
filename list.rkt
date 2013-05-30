896c7d0ee7fb420d77be6b01b9f7ff56e48da756
creasyw <creasywuqiong@gmail.com>
2013-05-30 00:25:19 -0500

    change api of list function in racket

--- list.rkt
+++ list.rkt
@@ -1,24 +1,10 @@
 #lang racket
-(provide num-to-intlist)
-(provide max-element)
-(provide min-element)
+(provide num->intlst)
 
 ;; returns a list of integers containing every digit of the "num"
-(define (num-to-intlist num)
+(define (num->intlst num)
   (let ((temp (string->list (number->string num))))
     (define (helper lst acc)
       (if (null? lst) (reverse acc)
           (helper (cdr lst) (cons (- (char->integer (car lst)) (char->integer #\0)) acc))))
     (helper temp '())))
-
-(define (max-element lst)
-  (define (helper result rest)
-    (if (null? rest) result
-        (helper (max result (car rest)) (cdr rest))))
-  (helper -inf.0 lst))
-
-(define (min-element lst)
-  (define (helper result rest)
-    (if (null? rest) result
-        (helper (min result (car rest)) (cdr rest))))
-  (helper +inf.0 lst))
