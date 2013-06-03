ba2b75c2e633cbe43af39ba8193342ebf3da9a90
creasyw <creasywuqiong@gmail.com>
2013-06-03 00:15:11 -0500

    add prime factor generation

--- factor.rb
+++ factor.rb
@@ -1,6 +1,12 @@
+require "Prime"
+
 def factors(n)
   # generate all factors divisible by n
   return (1..n).to_a.select{|x| n%x==0}
+end
+
+def prime_factors(n)
+  return  factors(n).select{|k| k.prime?}
 end
 
 
