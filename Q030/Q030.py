40d05d49520c5cac1bc3877ded553d30e9240611
creasyw <creasywuqiong@gmail.com>
2013-06-04 22:02:19 -0500

    modify python solution of q030

---
+++ Q030/Q030.py
@@ -0,0 +1,13 @@
+import sys
+
+def is_equal(num, power):
+    return num == sum(int(i)**power for i in str(num))
+
+def main ():
+    ub = 999999
+    power = 5
+    print sum(i for i in range(10, ub) if is_equal(i, power))
+    
+
+if __name__ == "__main__":
+    main()
