ee9562872602c0e989185325cf6c5f2786e617fa
creasyw <creasywuqiong@gmail.com>
2013-06-13 22:02:22 -0500

    add racket solution to q038

---
+++ Q038/Q038.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+;; import num->intlist
+(require "../list.rkt")
+
+(define (pandigital? n)
+  (let ((nlist (range 1 10))
+        (target (sort (num->intlst n) <)))
+    (equal? target nlist)))
+
+(define (pan-multiples? n)
+  ;; any number in condition will be regarded as true
+  ;; so this helper function returns either false or concatenated number
+  (define (helper count acc)
+    (let ((len (length acc)))
+      (cond ((and (= len 9) (equal? (range 1 10) (sort acc <))) (intlst->num acc))
+            ((>= len 9) #f)
+            (#t (helper (+ count 1) (append acc (num->intlst (* count n))))))))
+  (helper 1 '()))
+
+(define (main)
+  (let ((limit 10000000))
+    (define (helper count result)
+      (let ((returned (pan-multiples? count)))
+        (cond ((> count limit) result)
+              (returned (helper (+ count 1) (max result returned)))
+              (#t (helper (+ count 1) result)))))
+    (helper 2 0)))
+
+(main)
