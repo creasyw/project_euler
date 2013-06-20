48ed7a116a89220938a8a08b3754be35469a4786
creasyw <creasywuqiong@gmail.com>
2013-06-19 23:00:28 -0500

    add is_pythagorean function

--- factor.rb
+++ factor.rb
@@ -9,5 +9,13 @@ def prime_factors(n)
   return  factors(n).select{|k| k.prime?}
 end
 
+# because ruby cannot load file which load another file
+# I put the number operation in the same file with factors
+def is_pythagorean (premeter)
+  half = premeter / 2
+  m = factors(half).select{|k| 0<(half/k-k) and (half/k-k)<k and half/k>k}
+  return m.map{|k| [k, half/k-k]}.map{|k| [k.first**2-k.last**2,
+    2*k.first*k.last, k.first**2+k.last**2]}
+end
 
 
