53ba08f99b914ebe8be76a06820a26406712c4e0
creasyw <creasywuqiong@gmail.com>
2013-06-26 00:19:27 -0500

    dd three methods extending matrix class

--- matrix_extend.rb
+++ matrix_extend.rb
@@ -1,4 +1,4 @@
-require "matrix"
+require "Matrix"
 
 module Matrix_extend
     class :: Matrix
@@ -10,6 +10,27 @@ module Matrix_extend
                 result << self[i,i]
             end
             return result
+        end
+        
+        def Matrix.from_a(arr)
+            # build matrix based on input array
+            return Matrix[*arr]
+        end
+
+        def Matrix.vstack(m1, m2)
+            return Matrix[*m1, *m2]
+        end
+
+        def Matrix.hstack(m1, m2)
+            raise "The two input matrices have different dimensions"
+            unless m1.row_size == m2.row_size or
+                   m1.column_size == m2.column.size
+            end
+            result = []
+            m1.row_size.times do |i|
+                result << m1.row(i)+m2.row(i)
+            end
+            return Matrix[*result]
         end
     end
 end
