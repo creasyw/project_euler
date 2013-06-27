f53dd7aa907dbf65100fc9dc225766e329f1abf6
creasyw <creasywuqiong@gmail.com>
2013-06-26 22:45:47 -0500

    add matrix mathod rotating 90 degree

--- matrix_extend.rb
+++ matrix_extend.rb
@@ -1,38 +1,66 @@
 require "Matrix"
 
 module Matrix_extend
-    class :: Matrix
-        def diagonal()
-            # concatenate the lagest possible square matrix from the object
-            # and return its diagonal elements in a array
-            result = []
-            [self.row_size(), self.column_size()].min.times do |i|
-                result << self[i,i]
-            end
-            return result
-        end
-        
-        def Matrix.from_a(arr)
-            # build matrix based on input array
-            return Matrix[*arr]
-        end
-
-        def Matrix.vstack(m1, m2)
-            return Matrix[*m1, *m2]
-        end
-
-        def Matrix.hstack(m1, m2)
-            raise "The two input matrices have different dimensions"
-            unless m1.row_size == m2.row_size or
-                   m1.column_size == m2.column.size
-            end
-            result = []
-            m1.row_size.times do |i|
-                result << m1.row(i)+m2.row(i)
-            end
-            return Matrix[*result]
-        end
+  class :: Matrix
+    def diagonal
+      # concatenate the lagest possible square matrix from the object
+      # and return its diagonal elements in a array
+      result = []
+      [self.row_size(), self.column_size()].min.times do |i|
+	  result << self[i,i]
+      end
+      result
     end
+    
+    def rotate
+      cn = self.column_size
+      result = []
+      cn.times do |i| result<<self.column(cn-1-i) end
+      Matrix[*result]
+    end
+
+    def Matrix.from_a(arr)
+      # build matrix based on input array
+      Matrix[*arr]
+    end
+
+    #
+    # Create a matrix by vertically merge two existing matrix
+    #
+    #   x = Matrix[[1, 2], [3, 4]]
+    #   y = Matrix[[5, 6], [7, 8]]
+    #   Matrix.vstack(x, y)
+    #   => Matrix[[1, 2], [3, 4], [5, 6], [7, 8]]
+    #
+    def Matrix.vstack(m1, m2)
+      if  m1.column_size != m2.column_size
+	raise ArgumentError,
+	  "The two input matrices have different dimensions"
+      end
+      Matrix[*m1, *m2]
+    end
+
+    #
+    #
+    # Create a matrix by horizontally merge two existing matrix
+    #
+    #   x = Matrix[[1, 2], [3, 4]]
+    #   y = Matrix[[5, 6], [7, 8]]
+    #   Matrix.hstack(x, y)
+    #   => Matrix[[1, 2, 5, 6], [3, 4, 7, 8]]
+    #
+    def Matrix.hstack(m1, m2)
+      if  m1.row_size != m2.row_size
+	raise ArgumentError, 
+	  "The two input matrices have different dimensions"
+      end
+      result = []
+      m1.row_size.times do |i|
+	  result << m1.row(i).to_a+m2.row(i).to_a
+      end
+      Matrix[*result]
+    end
+  end
 end
 
 
