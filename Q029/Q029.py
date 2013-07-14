176d2b71f3f4c76812b327131486f1846f079e36
creasyw <creasywuqiong@gmail.com>
2013-07-13 23:57:11 -0500

    merge python solution

--- Q029/Q029.py
+++ Q029/Q029.py
@@ -1,12 +1,1 @@
-
-
-def square_sequence(ar, br):
-    result = set([])
-    for a in range(2,ar):
-        for b in range(2, br):
-            result.add(a**b)
-    return len(result)
-
-ar = 100
-br = 100
-print square_sequence(ar+1, br+1)
+print len(set([a**b for a in range(2, 101) for b in range(2, 101)]))
