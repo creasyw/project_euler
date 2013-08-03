12ff302554cf20f14dba023a3d1d9b915d57ea87
creasyw <creasywuqiong@gmail.com>
2013-08-03 00:13:05 -0500

    add racket solution to q064

---
+++ Q064/Q064.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+;; there is no need for function finding looping fragment
+;; because the looping can be uniquely defined by the pair (p, q)
+;; which define the loop formula (/ (- (sqrt n) p) q)
+(define (find-looper n)
+  (if (= (sqrt n) (integer-sqrt n)) 0
+      (letrec ((sq (sqrt n)))
+        (define (helper p q acc)
+          (if (member (list p q) acc)
+              (- (length acc) 1)
+              (letrec ((temp (floor (/ (* q (+ sq p)) (- n (* p p)))))
+                       (qp (/ (- n (* p p)) q))
+                       (pp (- (* qp temp) p)))
+                (helper pp qp (cons (list p q) acc)))))
+        ;; the initial value for q is 1.0 rather than 1
+        ;; because the (member) need to match both value and type
+        (helper (integer-sqrt n) 1.0 (list)))))
+
+(define (q064 limit)
+  (foldl + 0
+         (for/list ((i (in-range 2 (+ limit 1))))
+           (if (odd? (find-looper i)) 1 0))))
+
+(q064 10000)
