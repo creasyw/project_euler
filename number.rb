a1492107e2deae4aeffba43666f27aaad20d6dcf
creasyw <creasywuqiong@gmail.com>
2013-08-20 20:11:30 -0400

    add function num_of_pythagorean

--- number.rb
+++ number.rb
@@ -1,14 +1,25 @@
 require_relative "./factor.rb"
 
+# The explanation refers to number.rkt
 def is_pythagorean (premeter)
   half = premeter / 2
   m = factors(half).select{|k| 0<(half/k-k) and (half/k-k)<k and half/k>k}
-  return m.map{|k| [k, half/k-k]}.map{|k| [k.first**2-k.last**2,
+  m.map{|k| [k, half/k-k]}.map{|k| [k.first**2-k.last**2,
     2*k.first*k.last, k.first**2+k.last**2]}
+end
+
+# The explanation refers to number.rkt
+def num_of_pythagorean (num)
+  candidates = factors(num).drop(1).select{|i| is_pythagorean(i).length!=0}
+  (0..candidates.length-1).each{|i| if candidates[i]!=1
+    (i..candidates.length-1).each{|j|
+      temp=Math.log2(candidates[j]/candidates[i])
+      if temp!=0 and temp.to_i==temp then candidates[j]=1 end}
+  end}
+  candidates.select{|i| i!=1}.length
 end
 
 def factorial (st, en)
   (st..en).reduce(:*)
 end
-
 
