760ec797cc87fee0ce19bdfb6c56509be6cf651c
creasyw <creasywuqiong@gmail.com>
2013-09-16 11:37:23 -0500

    change func structure

--- primes.rb
+++ primes.rb
@@ -1,4 +1,6 @@
 
+
+
 def gen_prime (n)
   candidates = (1..n).step(1).to_a
   final = (2..Math.sqrt(n).round+1).step(1).to_a
@@ -8,7 +10,7 @@ def gen_prime (n)
   return candidates[1..-1].collect{|k| k if k}.compact
 end
 
-
-num = 1000
-puts "Under #{num}, found #{gen_prime(num).length} prime numbers" 
-
+def testing ()
+  num = 1000
+  puts "Under #{num}, found #{gen_prime(num).length} prime numbers" 
+end
