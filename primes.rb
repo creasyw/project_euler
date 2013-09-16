74e23bf384aebdeaeb7ab7186d250757ce2c4478
creasyw <creasywuqiong@gmail.com>
2013-09-16 11:55:20 -0500

    add coprime func

--- primes.rb
+++ primes.rb
@@ -1,6 +1,6 @@
+require "Prime"
 
-
-
+# this function is duplicated with the func in Prime..
 def gen_prime (n)
   candidates = (1..n).step(1).to_a
   final = (2..Math.sqrt(n).round+1).step(1).to_a
@@ -14,3 +14,8 @@ def testing ()
   num = 1000
   puts "Under #{num}, found #{gen_prime(num).length} prime numbers" 
 end
+
+def coprime(n)
+  (1..n-1).select{|i| n.gcd(i)==1}
+end
+
