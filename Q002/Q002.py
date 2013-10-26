9cf1ef58db3341727ca270104f073bd0fe1c6948
creasyw <creasywuqiong@gmail.com>
2013-10-26 13:48:16 -0500

    add sol in python

--- Q002/Q002.py
+++ Q002/Q002.py
@@ -12,6 +12,12 @@ def main (limit):
         if c%2 == 0: result += c
     print result
 
+def sol2(limit):
+    # without tail recursion, this method would exhaust ram..
+    fabs = [1,1]
+    [fabs.append(fabs[-1]+fabs[-2]) for i in range(limit)]
+    print sum(filter(lambda x: x%2==0, fabs))
+
 if __name__ == "__main__":
     main(4000000)
 
