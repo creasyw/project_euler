f73d5210687222686d2c1bc9c85a908420d0225a
creasyw <creasywuqiong@gmail.com>
2013-10-28 16:28:52 -0400

    add factor for huge number

--- factor.hs
+++ factor.hs
@@ -1,7 +1,13 @@
 module Factor where
 
+import Data.List -- for sort
 import Data.Numbers.Primes
 
 factor n = filter (\x -> n `mod` x == 0) [1..n]
+
+-- this is more efficient for huge number
+-- but twisted in expression and added overhead for several iterations
+factor1 :: Integral a => a -> [a]
+factor1 n = nub $ sort $ concatMap (\x -> [x, n `div` x]) [x | x<-[1.. round $ sqrt $ fromIntegral n], n `mod` x ==0]
 
 primeFactor n = filter isPrime (factor n)
