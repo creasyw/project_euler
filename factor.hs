6df47bd959288240d5720820b97aa27bd91125be
creasyw <creasywuqiong@gmail.com>
2014-01-09 19:10:29 -0600

    update factor func

--- factor.hs
+++ factor.hs
@@ -10,4 +10,10 @@ factor n = filter (\x -> n `mod` x == 0) [1..n]
 factor1 :: Integral a => a -> [a]
 factor1 n = nub $ sort $ concatMap (\x -> [x, n `div` x]) [x | x<-[1.. round $ sqrt $ fromIntegral n], n `mod` x ==0]
 
+-- nub is to eliminate duplicated items in a list with complexity O(n^2)
+-- the following func do a further optimization
+factor2 n = sort $ concatMap (\x -> if x^2/=n then [x, n `div` x] else [x])
+            [x | x<-[1.. round $ sqrt $ fromIntegral n], n `mod` x ==0]
+
+
 primeFactor n = filter isPrime (factor n)
