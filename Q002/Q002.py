dc29a9a1796ba375d05cff9ff51b644826f54457
creasyw <creasywuqiong@gmail.com>
2014-05-16 21:06:01 -0500

    add matrix sol find nth Fib in logN time

--- Q002/Q002.py
+++ Q002/Q002.py
@@ -1,3 +1,28 @@
+
+import numpy as np
+from functools import reduce
+
+def compose(*functions):
+    return reduce(lambda f, g: lambda x: f(g(x)), functions)
+
+def fib_matrix(n):
+    # basic matrix is [[1,1],[1,0]]
+    x = np.ones((2,2), dtype=int)
+    x[1,1] = 0
+    times = n
+    f1 = lambda i: np.dot(i, x)
+    f2 = lambda i: np.dot(i, i)
+    y = lambda i: i
+    while times!=1:
+        if times%2:
+            y = compose(y, f1, f2)
+        else:
+            y = compose(y, f2)
+        times /= 2
+    return y(x)[0,0]
+
+
+
 # calculate the nth Fibonacci number via Dynamic Programming
 def fib(n):
     a = 0
