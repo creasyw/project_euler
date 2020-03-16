835ac71a01f7c8df1123c12694a9e85d51f5cfea
Qiong Wu <qiong@eero.com>
2020-03-16 13:22:35 -0700

    Update note

--- Q004/Q004.hs
+++ Q004/Q004.hs
@@ -5,6 +5,11 @@
 isPalindrome :: (Integral a, Show a) => a -> Bool
 isPalindrome x = (show x) == (reverse (show x))
 
+-- I guess there is some smart way to find the numbers in a list that their
+-- products are in decreasing order. But the complexity for all three digits
+-- numbers are higher than simply trying them in segment. That is, calling this
+-- function by 900 - 999, 800 - 899, and so on if the previous segments fail to
+-- find any palindrome.
 largestProductPalindrome :: (Integral a, Show a) => a -> a -> a
 largestProductPalindrome x y =
     maximum . filter (isPalindrome) $ [ a * b | a <- range, b <- range ]
