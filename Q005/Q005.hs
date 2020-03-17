ba8f9653d821e70e9904bc238e5de937962af247
Qiong Wu <qiong@eero.com>
2020-03-17 09:52:20 -0700

    Add better solution

--- Q005/Q005.hs
+++ Q005/Q005.hs
@@ -1,6 +1,6 @@
-import Data.List as List
-import Data.Map.Strict as Map
-import Data.Numbers.Primes as Primes
+import           Data.List           as List
+import           Data.Map.Strict     as Map
+import           Data.Numbers.Primes as Primes
 
 -- The general thought is different from (and probably sub-optimal to) the
 -- Python/Racket algorithm. Basically, it counts the number of prime factors in
@@ -26,3 +26,26 @@ smallestDivisible a b = Map.foldlWithKey
     (\acc key val -> acc * (key ^ val))
     1
     (countFactorRange a b)
+
+
+-- 1. this is a much better solution
+-- 2. the typeclass in Haskell is killing me. =10 `div` 2= gets a =5= in =Int=,
+-- but =10/2= gets a =5.0= which is a =Fractional=. I haven't figuer out an
+-- elegant way to convert it back to =Integral=. =ceiling=, =floor=, =truncate=,
+-- =round= would somehow introduce a =RealFrac= type into the signature of the
+-- function =recursiveReduce= which makes it in turn uncompatible with =foldl=.
+--
+-- This algorithm iterates from the smallest element to the largest one. It
+-- avoids deducing prime numbers as well as avoiding using map. The basic idea
+-- is that assuming we need the range from a to b, the product of this range
+-- should only contain /one/ a. For all other numbers with the range (e.g. 2a,
+-- 3a, 4a...), the factor a can be removed.
+smallestDivisible' :: Integral a => a -> a -> a
+smallestDivisible' x y = List.foldl (*) 1 (recursiveReduce [x .. y])
+  where
+    recursiveReduce [] = []
+    recursiveReduce (x : xs) =
+        x
+            : (recursiveReduce
+                  (List.map (\k -> if k `mod` x == 0 then k `div` x else k) xs)
+              )
