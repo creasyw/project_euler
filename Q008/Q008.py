99b0de16b83a955df2f055890a61e45243530a20
Qiong Wu <qiong@eero.com>
2020-03-21 22:02:05 -0700

    Update sol for q8

--- Q008/Q008.py
+++ Q008/Q008.py
@@ -1,21 +1,28 @@
+import functools
 import os
 
-def largest_product (lst):
-    result = 0
-    for i in range(len(lst)-5):
-        temp = reduce(lambda x, y: x*y, lst[i:(i+5)],1)
-        result = max(result, temp)
+# we can always do `reduce` for the given range, but the complexity becomes O(M*N), where M is the
+# length of the input number and N is the range. On the other hand, if we only operate on the two
+# numbers that is about to discard and about to add, the complexity becomes O(M). It is independent
+# from the range.
+#
+# The only tricky part is the number zero.
+def largest_product(lst, count):
+    current = result = functools.reduce(lambda x, y: x * y, lst[:count], 1)
+    for i in range(len(lst)-count):
+        if lst[i] == 0:
+            current = functools.reduce(lambda x, y: x * y, lst[(i+1):(i+count+1)], 1)
+        else:
+            current = current // lst[i] * lst[i+count]
+        result = max(result, current)
     return result
 
 def main():
-    lst = []
-    with open(os.path.join(os.path.dirname(__file__), "Q008.txt")) as datafile:
-        for row in datafile:
-            # the last character of each line is line break
-            lst += [int(k) for k in row[:-1]]
-    print largest_product(lst)
+    register, count = "", 13
+    with open(os.path.join(os.path.dirname(__file__), "Q008.txt")) as handler:
+        for line in handler:
+            register += line
+    print(largest_product([int(k) for k in register[:-1]], count))
 
 if __name__ == "__main__":
     main()
-
-
