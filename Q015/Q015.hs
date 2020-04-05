9a7f4f0a4703b8230846671e3088588232c5322d
Qiong Wu <qiong@eero.com>
2020-04-04 18:59:53 -0700

    Update commnet

--- Q015/Q015.hs
+++ Q015/Q015.hs
@@ -1,3 +1,8 @@
+-- struggled a bit on the signature of the helper function (or the actual
+-- implementation of the =scanl=). =scanl= use the initiate value as the first
+-- element in the generated list. Also, I paused a bit thinking about the
+-- iterations in Haskell and finally realized that all loops can be converted to
+-- recursive function.
 
 numRoutes num = last $ helper 2 (replicate num 1)
   where
