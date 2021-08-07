ddef8d5eb830f64362cc4aece3b2f6809e93d599
Qiong Wu <qiong@eero.com>
2021-08-06 20:53:40 -0700

    Add comments

--- Q027/Q027.hs
+++ Q027/Q027.hs
@@ -1,11 +1,15 @@
 import Data.Numbers.Primes as P
 
 
+-- Good to have the ability working with infinite list
 countPrimes :: (Int -> Int) -> Int
 countPrimes func = length $ takeWhile (P.isPrime . func) [0..]
 
+-- the anonymous function is more of a dispatch between =foldl= and the helper
+-- function - feed two tuples into a function and get the resulting tuple as
+-- accumulation.
 longestPrimes :: (Int, Int)
-longestPrimes = foldl (\(acc, product) (a, b) -> (helper acc product a b))
+longestPrimes = foldl (\(acc, product) (a, b) -> helper acc product a b)
                 (0, 0)
                 [(x, y) | x <- [-999..999], y <- [2..999]]
   where helper acc product a b = if acc < newVal
